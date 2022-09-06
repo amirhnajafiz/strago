f56253e3acdfa81b50606ac951e1e9d66d9ad51a
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-06 20:34:57 +0430

    add: gin handler

--- internal/server.go
+++ internal/server.go
@@ -1,10 +1,25 @@
 package internal
 
-import "net/http"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 type server struct {
 }
 
-func (s *server) register() {
-	http.HandleFunc("/*", handleRequests)
+func (s *server) Start(port int) error {
+	address := ":" + strconv.Itoa(port)
+	app := gin.Default()
+
+	gin.SetMode(gin.ReleaseMode)
+	app.Use(handleRequests)
+
+	if err := app.Run(address); err != nil {
+		return fmt.Errorf("register server failed: %w", err)
+	}
+
+	return nil
 }
