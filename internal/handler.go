53ff80bfd290edb31be4a70c12e8f220eacdf7ff
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-13 08:46:18 +0430

    add: ip return method

--- internal/handler.go
+++ internal/handler.go
@@ -2,9 +2,20 @@ package internal
 
 import (
 	"net/http"
+	"sort"
 
 	"github.com/gin-gonic/gin"
 )
+
+func (s *server) getIP() string {
+	ip := s.services[0]
+
+	sort.Slice(s.services, func(i, j int) bool {
+		return i < j
+	})
+
+	return ip
+}
 
 func (s *server) handleRequests(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, ctx.Request)
