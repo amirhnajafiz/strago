ca2ad21df209622456153a5d692c86817c939ea8
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-15 08:28:44 +0430

    bug: in getip

--- example/main.go
+++ example/main.go
@@ -3,7 +3,13 @@ package main
 import "github.com/amirhnajafiz/strago"
 
 func main() {
-	server := strago.NewServer(strago.WithServices(strago.WithDefaultConfigs(), "127.0.0.1"))
+	server := strago.NewServer(
+		strago.WithServices(
+			strago.WithDefaultConfigs(),
+			"127.0.0.1:5050",
+			"127.0.0.1:5051",
+		),
+	)
 
 	if err := server.Start(); err != nil {
 		panic(err)
