e4475aa6d44b6c0cbe3aec5b236b1f21a78caf49
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-15 08:47:00 +0430

    add: info for pkg

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,9 +1,27 @@
 package logger
 
-import "go.uber.org/zap"
+import (
+	"os"
 
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// NewLogger
+// creates a new zap logger interface.
 func NewLogger() *zap.Logger {
-	logger, _ := zap.NewProduction()
+	level := zapcore.DebugLevel
+
+	encoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+
+	defaultCore := zapcore.NewCore(encoder, zapcore.Lock(zapcore.AddSync(os.Stderr)), level)
+	cores := []zapcore.Core{
+		defaultCore,
+	}
+
+	core := zapcore.NewTee(cores...)
+
+	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
 
 	return logger
 }
