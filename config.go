008ca103b59b90bf4eed350b40192eb395a33287
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-15 08:42:40 +0430

    add: description for config and server

--- config.go
+++ config.go
@@ -1,20 +1,20 @@
 package strago
 
+// Config
+// is strago server configs.
 type Config struct {
-	Enable   bool
-	Port     int
+	// Enable server or disable it.
+	Enable bool
+	// Server Port.
+	Port int
+	// Services which is a list of available services.
 	Services []string
-	Type     string
+	// Service Type which can be http or https.
+	Type string
 }
 
-func ProductionConfigs() *Config {
-	return &Config{
-		Enable: true,
-		Port:   9370,
-		Type:   "https",
-	}
-}
-
+// WithDefaultConfigs
+// returns a default config set of strago.
 func WithDefaultConfigs() *Config {
 	return &Config{
 		Enable: false,
@@ -23,13 +23,22 @@ func WithDefaultConfigs() *Config {
 	}
 }
 
+// ProductionConfigs
+// returns a set of configs for production of strago server.
+func ProductionConfigs() *Config {
+	return &Config{
+		Enable: true,
+		Port:   9370,
+		Type:   "https",
+	}
+}
+
+// WithServices
+// is used to attach services into strago config.
+// If you don't pass config, it will use default configs.
 func WithServices(config *Config, services ...string) *Config {
 	if config == nil {
-		cfg := WithDefaultConfigs()
-
-		cfg.Services = services
-
-		return cfg
+		config = WithDefaultConfigs()
 	}
 
 	config.Services = services
--- server.go
+++ server.go
@@ -4,14 +4,27 @@ import (
 	"github.com/amirhnajafiz/strago/internal"
 )
 
+// LoadBalancer
+// is the strago server which manages the
+// features that a load-balancer has.
 type LoadBalancer interface {
+	// Start server.
 	Start() error
+	// Open one of the services.
 	Open(ip string) error
+	// Close one of the services.
 	Close(ip string) error
 }
 
+// NewServer
+// creates a new load-balancer server.
 func NewServer(cfg *Config) LoadBalancer {
-	server := internal.NewServer(cfg.Enable, cfg.Port, cfg.Type, cfg.Services...)
+	server := internal.NewServer(
+		cfg.Enable,
+		cfg.Port,
+		cfg.Type,
+		cfg.Services...,
+	)
 
 	return server
 }
