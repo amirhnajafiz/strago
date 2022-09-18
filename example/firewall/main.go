a734752a8081312986df7f670d577e7c24140553
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-18 20:25:20 +0430

    update: readme

--- example/firewall/main.go
+++ example/firewall/main.go
@@ -3,15 +3,23 @@ package main
 import "github.com/amirhnajafiz/strago"
 
 func main() {
+	// creating a new strago sever
 	server := strago.NewServer(
 		strago.WithServices(
 			strago.WithDefaultConfigs(),
+			// given services ips
 			"127.0.0.1:5050",
 			"127.0.0.1:5051",
 		),
 	)
 
+	// ban any ip in range 127... version 4
 	if err := server.BanIP("127.*.*.*"); err != nil {
+		panic(err)
+	}
+
+	// ban any ip in range 12...12 version 6
+	if err := server.BanIP("12:*:*:*:22:*:*:*"); err != nil {
 		panic(err)
 	}
 
