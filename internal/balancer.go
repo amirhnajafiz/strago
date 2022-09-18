ba6a6a9ca58f615af403933cefebb095157779a7
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-18 19:53:54 +0430

    add: balancer

---
+++ internal/balancer.go
@@ -0,0 +1,21 @@
+package internal
+
+import (
+	"sort"
+)
+
+// getOneIPFromServices
+// returns one ip address from given services.
+// sorting type is based on number of requests, or
+// the busy time of a service.
+func (s *server) getOneIPFromServices() string {
+	serv := s.services[0]
+
+	serv.used++
+
+	sort.Slice(s.services, func(i, j int) bool {
+		return s.services[i].used < s.services[j].used
+	})
+
+	return serv.ip
+}
