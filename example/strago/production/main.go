e2da056fe9d835efd0cb2f311b83b2092fe46cdd
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-18 18:33:58 +0330

    update: examples

--- example/strago/production/main.go
+++ example/strago/production/main.go
@@ -3,16 +3,16 @@ package main
 import "github.com/amirhnajafiz/strago"
 
 func main() {
-	server := strago.NewServer(
-		strago.WithServices(
-			strago.ProductionConfigs(),
-			"127.0.0.1:5050",
-			"127.0.0.1:5051",
-		),
-	)
+	// creating a new server
+	server := strago.NewServer(strago.ProductionOptions())
 
+	// setting the services
+	server.WithServices("127.0.0.1:5050", "127.0.0.1:5051")
+
+	// closing one port
 	_ = server.Close("127.0.0.1:5050")
 
+	// starting server
 	if err := server.Start(); err != nil {
 		panic(err)
 	}
