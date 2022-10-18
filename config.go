624de683ff118cd09b9adc62916316b87b70819a
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-18 18:28:30 +0330

    remove: services from config

--- config.go
+++ config.go
@@ -1,24 +1,22 @@
 package strago
 
-// Config
+// Options
 // is strago server configs.
-type Config struct {
+type Options struct {
 	// Enable server or disable it.
 	Enable bool
 	// Server Port.
 	Port int
-	// Services which is a list of available services.
-	Services []string
 	// Service Type which can be http or https.
 	Type string
 	// BalancingType selects the parameter to balance services.
 	BalancingType int
 }
 
-// WithDefaultConfigs
+// WithDefaultOptions
 // returns a default config set of strago.
-func WithDefaultConfigs() *Config {
-	return &Config{
+func WithDefaultOptions() *Options {
+	return &Options{
 		Enable:        false,
 		Port:          9370,
 		Type:          "http",
@@ -26,26 +24,13 @@ func WithDefaultConfigs() *Config {
 	}
 }
 
-// ProductionConfigs
+// ProductionOptions
 // returns a set of configs for production of strago server.
-func ProductionConfigs() *Config {
-	return &Config{
+func ProductionOptions() *Options {
+	return &Options{
 		Enable:        true,
 		Port:          9370,
 		Type:          "https",
 		BalancingType: BusyTime,
 	}
-}
-
-// WithServices
-// is used to attach services into strago config.
-// If you don't pass config, it will use default configs.
-func WithServices(config *Config, services ...string) *Config {
-	if config == nil {
-		config = WithDefaultConfigs()
-	}
-
-	config.Services = services
-
-	return config
 }
